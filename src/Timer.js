112ffc048c3e68c37d4c7b2135e2f72fee54e807
AlexMinez <alex_minihan@outlook.com>
2022-05-12 14:13:28 +1000

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -7,6 +7,13 @@ class Timer extends Component {
   };
 
   // add your code here
+componentDidMount(){
+  this.interval = setInterval(this.clockTick, 1000);
+}
+componentWillUnmount(){
+  clearInterval(this.interval);
+}
+
 
   render() {
     const { time, color } = this.state;
@@ -36,5 +43,6 @@ class Timer extends Component {
     this.props.removeTimer(this.props.id);
   };
 }
+
 
 export default Timer;
